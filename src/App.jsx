7b02727286ddb36d08f17588f7f10392fff52ccc
Frosselly <11691962+Frosselly@users.noreply.github.com>
2024-09-11 23:17:42 +0300

    Elden ring API data fetch and format

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import CardHolder from './cardHolder';
 //data[] -> name, image(!!! can be null)
-const eldenRingAPI = "https://eldenring.fanapis.com/api/bosses?limit=100";
+const eldenRingAPI = "https://eldenring.fanapis.com/api/bosses?limit=50";
 //https://eldenring.fanapis.com/api/bosses?limit=20&page=3
 
 function App() {
   const [currScore, setCurrScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState([]);
 
   const card = {
     id: 0,
@@ -14,11 +16,60 @@ function App() {
     img: ""
   }
 
+  useEffect(() => {
+    getCardData(eldenRingAPI).then((data) => {
+      const formattedData = getData(data);
+      setCards(formattedData);
+      //console.log("data", formattedData);
+    });
+    
+  }, [])
+  console.log("data", cards);
   return (
     <>
-
+      <CardHolder data={cards}/>
     </>
   )
 }
 
 export default App
+
+
+function getData(data){
+  let formattedData = [];
+  let count = 0;
+  for(let entry of data){
+    if(count === 12) break;
+    if(entry.image === null) continue;
+    if(formattedData.find((obj) => { 
+      return compareWords(obj.name, entry.name) > 2;
+    })) continue;
+
+    formattedData.push({id:entry.id, name:entry.name, img:entry.image})
+    count++;
+  }
+  return formattedData;
+}
+
+async function getCardData(url) {
+  const response = await fetch(url, {mode: 'cors'});
+  return response.json().then(function(response) {
+    console.log(response.data);
+    return response.data;
+  });
+}
+
+const compareWords = (str1, str2) => {
+  const words1 = str1.split(/\s+/); // Split str1 into an array of words
+  const words2 = str2.split(/\s+/); // Split str2 into an array of words
+
+  let matchingWords = 0;
+
+  for(let word of words1){
+    if (words2.includes(word)) {
+      matchingWords++;
+    }
+  }
+
+  return matchingWords;
+};
--- src/cardHolder.jsx
+++ src/cardHolder.jsx
@@ -1,9 +1,20 @@
-
-
-function CardHolder(){
-
+export default function CardHolder({ data }) {
+  return (
+    <div>
+      {data.map((cardData) => (
+        <Card key={cardData.id} img={cardData.img} text={cardData.name} />
+      ))}
+    </div>
+  );
 }
 
-function Card(){
-    
+function Card({ img, text }) {
+  return (
+    <div>
+      <img src={img} />
+      <div>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
